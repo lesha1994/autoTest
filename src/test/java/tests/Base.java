9f6250e7875e3f1265f55b12a44434b9f4394e4d
Digriz <fiveadvert@gmail.com>
2022-11-27 12:54:20 +0100

    fixing

--- src/test/java/tests/Base.java
+++ src/test/java/tests/Base.java
@@ -1,12 +1,14 @@
 package tests;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 import java.io.File;
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public  class Base  {
     public static WebDriver driver;
@@ -15,24 +17,19 @@ public  class Base  {
 
     }
 
-    @BeforeClass
+    @BeforeSuite
     public void setup(){
         File file = new File("src/main/resources/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://qa-pos.b2bsoft.com/");
-
     }
 
-
-
-
-    //@AfterClass
-    //public void finish(){
-      //  driver.quit();
-    //}
+    @AfterClass
+    public void finish(){
+        driver.quit();
+    }
 
 }
